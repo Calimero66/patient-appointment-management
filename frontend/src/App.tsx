b9cc00e9c86e210ff5619d52cf64aaf1cb267c57
Hamza Aboulmaali <hamza.aboulmaali99@gmail.com>
2026-09-09 17:24:53 +0100

    new routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,7 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
-import { AuthProvider } from './context/AuthContext';
-import { Login } from './pages/Login';
-import { Register } from './pages/Register';
-import { Dashboard } from './pages/Dashboard';
-import { ProtectedRoute } from './components/ProtectedRoute';
-import { GuestRoute } from './components/GuestRoute';
+import AppRouter from './router';
 
 function App() {
-  return (
-    <AuthProvider>
-      <Toaster position="top-right" reverseOrder={false} />
-      <BrowserRouter>
-        <Routes>
-          {/* Guest / Public-Only Routes (Redirect logged-in users to /dashboard) */}
-          <Route element={<GuestRoute />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Route>
-
-          {/* Protected Super Admin / System User Dashboard Route */}
-          <Route element={<ProtectedRoute />}>
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Route>
-
-          {/* Catch-all redirect */}
-          <Route path="*" element={<Navigate to="/dashboard" replace />} />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
-  );
+  return <AppRouter />;
 }
 
 export default App;
